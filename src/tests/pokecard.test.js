80f3cbf93a749d7a041f20e49f859a1e36b4a0f6
WeslleiBrito <82123233+WeslleiBrito@users.noreply.github.com>
2023-09-19 11:15:55 -0300

    Exercicio 3

--- src/tests/pokecard.test.js
+++ src/tests/pokecard.test.js
@@ -1,10 +1,12 @@
 import { pokemonApiMock } from "./mocks/pokemonMock";
 import { render, screen, waitFor } from '@testing-library/react';
 import axios from 'axios'; // temos que importar o módulo aqui também
+import userEvent from "@testing-library/user-event";
 import Pokecard from "../components/Pokecard";
 
 jest.mock('axios')
-const openModal = jest.fn()
+const openModalMock = jest.fn()
+
 
 describe("Testando o ProductCard", () => {
     
@@ -16,8 +18,21 @@ describe("Testando o ProductCard", () => {
     test('Deve renderizar os elementos da pokecard', async () => {
         axios.get.mockResolvedValueOnce(pokemonApiMock)
 
-        render(<Pokecard/>)
+        render(<Pokecard />)
         await waitFor(() => {})
+
+        screen.logTestingPlaygroundURL()
+    })
+
+    test('Deve executar a função que habilita o modal', async () => {
+        const user = userEvent.setup()
+        axios.get.mockResolvedValueOnce(pokemonApiMock)
+
+        render(<Pokecard openModal={openModalMock}/>)
+        await waitFor(() => {})
+
+        const card = screen.getByRole('article')
+        await user.click(card)
 
         screen.logTestingPlaygroundURL()
     })
